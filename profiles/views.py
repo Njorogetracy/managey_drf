e058e09c2ba99d22d2f745f634ba2fd3761d8eef
Hope Tracy <hnjoroge64@gmail.com>
2024-05-22 21:42:06 +0000

    Add: add profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,17 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
 
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
