7e3bec406ef6fb3d8b85857c1912b796f1eb9db0
Hope Tracy <hnjoroge64@gmail.com>
2024-05-23 15:28:46 +0000

    Add: add login/logout view, permissions to owner profile, and isowner field to serializer

--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,14 @@ from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.http import Http404
+from managey_drf.permissions import IsOWnerorReadOnly
 
-class ProfileList(generics.ListCreateAPIView):
+class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOWnerorReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
