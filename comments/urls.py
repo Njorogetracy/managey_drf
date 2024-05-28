186fc63fd34213dcb05c83d224c5c7975af6d189
Hope Tracy <hnjoroge64@gmail.com>
2024-05-28 21:01:41 +0000

    add: create commentsdetailview for users to update and delete comments

--- comments/urls.py
+++ comments/urls.py
@@ -3,4 +3,5 @@ from comments import views
 
 urlpatterns = [
     path('comments/', views.CommentsList.as_view()),
+    path('comments/<int:pk>/', views.CommentsDetail.as_view()),
 ]
--- comments/views.py
+++ comments/views.py
@@ -10,3 +10,9 @@ class CommentsList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CommentsDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOWnerorReadOnly]
+    serializer_class = CommentsSerializer
+    queryset = Comments.objects.all()
