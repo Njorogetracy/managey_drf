b62e2163124418b49315632c60807723b773fe16
Hope Tracy <hnjoroge64@gmail.com>
2024-05-29 10:40:04 +0000

    add: add django backend filters, filter comments by task and owner

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from managey_drf.permissions import IsOWnerorReadOnly
 from .models import Comments
 from .serializers import CommentsSerializer, CommentsDetailSerializer
@@ -7,6 +8,8 @@ class CommentsList(generics.ListCreateAPIView):
     serializer_class = CommentsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comments.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['task', 'owner']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
