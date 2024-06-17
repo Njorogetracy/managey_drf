f8a8216dd00db2d0be5943029e309ce0b3c09ed3
Hope Njoroge <hnjoroge64@gmail.com>
2024-06-17 12:35:53 +0000

    update profileserializer to allow users to view tasks they have assigned to other users

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Profile
+from tasks.models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Task
+        fields =['id', 'title', 'description', 'due_date', 'priority', 'state']        
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
@@ -8,13 +14,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     tasks_count = serializers.ReadOnlyField()
+    tasks = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_tasks(self, obj):
+        request = self.context['request']
+        if request.user.is_authenticated:
+            tasks = Task.objects.filter(assigned_users=obj.owner, owner=request.user)
+            return TaskSerializer(tasks, many=True).data 
+        return []
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'bio', 'image', 'is_owner', 'tasks_count',
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'bio', 'image', 'is_owner', 'tasks_count', 'tasks',
         ]
